1ff3e0126d19827103ca70c9eceb9ad58e799591
ToqaTarek <106085045+ToqaTarek@users.noreply.github.com>
2023-01-15 20:57:51 +0200

    Delete website_php directory

--- website_php/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost","root","") or die ("Error: can't connect to Server");
-$db = mysqli_select_db($con, "booking_courts_db") or die ("Error: can't connect to Database");
-
-?>
